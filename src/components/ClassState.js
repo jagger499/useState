ec02d62fa0421aec002ccab644f44b6bc21baf71
jhon garcia <jagg499@gmail.com>
2022-01-22 18:12:20 -0500

    add validation to a code width states error and loading

--- src/components/ClassState.js
+++ src/components/ClassState.js
@@ -1,11 +1,15 @@
 import React from "react";
 import { Loading } from "./loading";
 
+
+const SECURITY_CODE = 'paradigma';
+
 class ClassState extends React.Component {
   constructor (props) {
     super(props);
 
     this.state = {
+      value: '',
       error: false,
       loading: false,
     };
@@ -15,6 +19,12 @@ class ClassState extends React.Component {
     if(this.state.loading) {
       setTimeout(() => {
         this.setState({ loading : false});
+
+        if( this.state.value === SECURITY_CODE){
+          this.setState({ error:false, loading: false });
+        } else {
+          this.setState({ error: true, loading: false })
+        }
       }, 3000)
     }
   }
@@ -24,21 +34,17 @@ class ClassState extends React.Component {
       <div>
         <h2>Eliminar {this.props.name}</h2>
         <p>por favor escribe el codigo de seguridad</p>
-        {
-          this.state.error && (
-            <p>error el codigo es incorrecto</p>
-          )
-        }
-        {
-          this.state.loading && (
-            <Loading />
-          )
-        }
+        { this.state.error && ( <p>error el codigo es incorrecto</p>)}
+        { this.state.loading && ( <Loading /> )}
 
-        <input placeholder={'codigo de seguridad'}/>
+        <input placeholder={'codigo de seguridad'}
+               value={this.state.value}
+               onChange={(e) => {
+                 this.setState({ value: e.target.value });
+               }}/>
         <button onClick={() => {
-          this.setState({ error: !this.state.error });
-          this.setState({ loading: !this.state.loading });
+          this.setState({ error: false });
+          this.setState({ loading: true });
         }}> Comprobar </button>
       </div>
     )
--- src/components/UseState.js
+++ src/components/UseState.js
@@ -1,34 +1,38 @@
 import React, {useEffect, useState} from "react";
 
 const UseState = ({ name }) => {
+  const [value,setValue] = useState('');
   const [error, setError] = useState(false);
   const [loading, setLoading] = useState(false);
 
+  const SECURITY_CODE = 'paradigma';
+
   useEffect(() => {
-    setTimeout(() => {
+    setTimeout(() => {  
       if (loading){
+        if ( value !== SECURITY_CODE ){
+          setError(true);
+        } 
         setLoading(false);
       }
     }, 3000)
+  // eslint-disable-next-line react-hooks/exhaustive-deps
   },[loading]);
 
   return (
       <div>
         <h2>Eliminar {name}</h2>
         <p>por favor escribe el codigo de seguridad</p>
-        { error && (
-          <p>Error el codigo es incorrecto</p>
-        ) 
-        }
-        {
-          loading && (
-            <p>cargando...</p>
-          )
-        }
-        <input placeholder={'codigo de seguridad'}/>
+        { error && (<p>Error el codigo es incorrecto</p>) }
+        { loading && (<p>cargando...</p>) }
+        <input placeholder={'codigo de seguridad'}
+               value={value}
+               onChange={(e) => {
+                 setValue(e.target.value);
+               }}/>
         <button onClick={() => {
-            setError( prevState => !prevState );
             setLoading( true );
+            setError( false );
         }}> Comprobar </button>
       </div>
     )
