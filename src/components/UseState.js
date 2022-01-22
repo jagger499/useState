aefc04220444c8a6e4fc2934dff62eefd6660e57
jhon garcia <jagg499@gmail.com>
2022-01-22 18:40:24 -0500

    add composite state to UseState component

--- src/components/UseState.js
+++ src/components/UseState.js
@@ -1,38 +1,44 @@
 import React, {useEffect, useState} from "react";
+const SECURITY_CODE = 'paradigma';
 
 const UseState = ({ name }) => {
-  const [value,setValue] = useState('');
-  const [error, setError] = useState(false);
-  const [loading, setLoading] = useState(false);
-
-  const SECURITY_CODE = 'paradigma';
+  const [state, setState] = useState({
+    value: '',
+    loading: false,
+    error: false,
+  });
 
   useEffect(() => {
     setTimeout(() => {  
-      if (loading){
-        if ( value !== SECURITY_CODE ){
-          setError(true);
-        } 
-        setLoading(false);
+      if (state.loading){
+        if ( state.value === SECURITY_CODE ){
+          // setError(true);
+          setState({ ...state, loading: false,  error: false });
+        } else {
+          setState({ ...state, loading: false, error: true });
+        }
+        // setLoading(false);
       }
     }, 3000)
   // eslint-disable-next-line react-hooks/exhaustive-deps
-  },[loading]);
+  },[state.loading]);
 
   return (
       <div>
         <h2>Eliminar {name}</h2>
         <p>por favor escribe el codigo de seguridad</p>
-        { error && (<p>Error el codigo es incorrecto</p>) }
-        { loading && (<p>cargando...</p>) }
+        { state.error && (<p>Error el codigo es incorrecto</p>) }
+        { state.loading && (<p>cargando...</p>) }
         <input placeholder={'codigo de seguridad'}
-               value={value}
+               value={state.value}
                onChange={(e) => {
-                 setValue(e.target.value);
+                 // setValue(e.target.value);
+                 setState({ ...state, value: e.target.value });
                }}/>
         <button onClick={() => {
-            setLoading( true );
-            setError( false );
+            // setLoading( true );
+            // setError( false );
+            setState({ ...state, loading: true, error: false });
         }}> Comprobar </button>
       </div>
     )
