bf4aa289d08b957e25a3375521adc7ed40ffeb93
jhon garcia <jagg499@gmail.com>
2022-01-25 22:31:29 -0500

    add actyonTypes and actyonType functions to have more declarative code

--- src/reducers/useReducer.js
+++ src/reducers/useReducer.js
@@ -4,15 +4,22 @@ import React, {useEffect, useReducer} from "react";
 const UseReducer = ({ name }) => {
   const [state, dispatch] = useReducer(useReducerObject , initialState)
 
+  const onConfirm = () => dispatch({ type: actyonTypes.confirm});
+  const onError = () => dispatch({ type: actyonTypes.error})
+  const onAccept = () => dispatch({ type: actyonTypes.accept })
+  const onWrite = (e) => dispatch({ type: actyonTypes.write, payload: e.target.value })
+  const onReset = () => dispatch({ type: actyonTypes.reset });
+  const onDeleted = () => dispatch({ type: actyonTypes.deleted });
+
   const SECURITY_CODE = 'paradigma';
 
   useEffect(() => {
     setTimeout(() => {  
       if (state.loading){
         if ( state.value === SECURITY_CODE ){
-          dispatch({ type: 'CONFIRM' });
+          onConfirm();
         } else {
-          dispatch({ type: 'ERROR' })
+          onError();
         }
       }
     }, 3000)
@@ -26,31 +33,24 @@ const UseReducer = ({ name }) => {
           <p>por favor escribe el codigo de seguridad</p>
           { state.error && (<p>Error el codigo es incorrecto</p>) }
           { state.loading && (<p>cargando...</p>) }
-          <input placeholder={'codigo de seguridad'}
-                 value={state.value}
-            onChange={e => dispatch({ type: 'WRITE', payload: e.target.value })}
+          <input  placeholder={'codigo de seguridad'}
+                  value={state.value} onChange={onWrite}
                 />
-          <button onClick={() => dispatch({ type: 'ACCEPT' })}> Comprobar </button>
+          <button onClick={onAccept}> Comprobar </button>
         </div>
       )
   } else if ( state.confirm && !state.deleted){
     return(
       <>
         <p>estado de confirmacion</p>
-        <button onClick={() => { dispatch({ type: 'DELETED' })}}>
-          Si eliminar
-        </button>
-        <button onClick={() => { dispatch({ type: 'RESET' })}}>
-          No me cague
-        </button>
+        <button onClick={onDeleted}>Si eliminar</button>
+        <button onClick={onReset}>No me cague</button>
       </>
     )
   } else {
     return(<>
       <p>Eliminado con exito</p>
-        <button onClick={() => { dispatch({ type: 'RESET' })}}>
-          volver al inicio
-        </button>
+        <button onClick={onReset}>volver al inicio</button>
     </>
     )
   }
@@ -65,14 +65,23 @@ const initialState = {
     confirm: false,
 }
 
+const actyonTypes = {
+  error : 'ERROR',
+  confirm: 'CONFIRM',
+  write: 'WRITE',
+  accept: 'ACCEPT',
+  reset: 'RESET',
+  deleted: 'DELETED',
+}
+
 // uso de reducer object dividiendo en 2 la forma en que se hace
 const reducerObject = (state, payload) => ({
-    'ERROR': { ...state, error:true, loading: false },
-    'CONFIRM': { ...state, loading: false, error: false, confirm: true },
-    'WRITE': { ...state, value: payload },
-    'ACCEPT': { ...state, loading: true, error:false },
-    'RESET': { ...state, deleted: false, confirm: false },
-    'DELETED':  {...state, deleted: true },
+    [actyonTypes.error] : { ...state, error:true, loading: false },
+    [actyonTypes.confirm]: { ...state, loading: false, error: false, confirm: true },
+    [actyonTypes.write]: { ...state, value: payload },
+    [actyonTypes.accept]: { ...state, loading: true, error:false },
+    [actyonTypes.reset]: { ...state, deleted: false, confirm: false },
+    [actyonTypes.deleted]:  {...state, deleted: true },
 })
 
 const useReducerObject = (state, action) => {
